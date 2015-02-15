2630309e9ac0269a2a34a6d216e3a2668eded9fe
beLive <ndenisenko@wayray.com>
2015-02-16 00:30:10 +0300

    add licence and comments

--- weather-api-owm/src/main/java/com/be1ive/weather/owm/api/impl/OpenWeatherMapTemplate.java
+++ weather-api-owm/src/main/java/com/be1ive/weather/owm/api/impl/OpenWeatherMapTemplate.java
@@ -1,5 +1,30 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 the original author or authors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.be1ive.weather.owm.api.impl;
 
+import com.be1ive.weather.AbstractApiConfigurations;
 import com.be1ive.weather.owm.api.CurrentConditionsOperations;
 import com.be1ive.weather.owm.api.DailyForecastOperations;
 import com.be1ive.weather.owm.api.HourlyForecastOperations;
@@ -7,7 +32,6 @@ import com.be1ive.weather.owm.api.OpenWeatherMap;
 import com.be1ive.weather.owm.api.OpenWeatherMapErrorHandler;
 import com.be1ive.weather.owm.api.ParametrisedList;
 import com.be1ive.weather.owm.api.impl.json.OpenWeatherMapModule;
-import com.be1ive.weather.AbstractApiConfigurations;
 import com.be1ive.weather.support.URIBuilder;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,7 +49,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Nikolay on 14.02.2015.
+ * @author Nikolay Denisenko
+ * @version 2015/02/16
  */
 public class OpenWeatherMapTemplate extends AbstractApiConfigurations implements OpenWeatherMap {
 
